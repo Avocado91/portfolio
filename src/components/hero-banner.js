acf8164813476c6c7f6e1e14fc2c84dfd2caa66d
T.J <t.ruggles@live.ca>
2018-12-23 00:12:56 -0500

    Adding hero-banner component

---
+++ src/components/hero-banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroBanner = () => (
+  <div>
+    <img src='#' alt='logo' />
+    <h1>T.J. Ruggles | Front-End Developer</h1>
+    <h3>I'm a Web Developer based out of Ottawa, Canada</h3>
+    <p>Learn more about what I do</p>
+    <p>arrow down</p>
+  </div>
+);
+
+export default HeroBanner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Header from './../components/header'
+import HeroBanner from './../components/hero-banner'
 
 export default () => (
   <div>
     <Header></Header>
+    <HeroBanner></HeroBanner>
   </div>
 );
