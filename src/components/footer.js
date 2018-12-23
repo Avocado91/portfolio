6f799acb9a74a6a6201be86073e461f5d94429d5
T.J <t.ruggles@live.ca>
2018-12-23 00:43:10 -0500

    Adding footer component

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+  <div>
+    <div>
+      <a href='#'>Github</a>
+      <a href='#'>Linkedin</a>
+      <a href='#'>contact</a>
+    </div>
+    <div>T.J. Ruggles - &copy 2018</div>
+  </div>
+);
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import Header from './../components/header'
 import HeroBanner from './../components/hero-banner'
 import About from './../components/about'
+import Footer from './../components/footer'
 
 export default () => (
   <div>
     <Header></Header>
     <HeroBanner></HeroBanner>
     <About></About>
+    <Footer></Footer>
   </div>
 );
