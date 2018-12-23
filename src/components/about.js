457b46120fbe3ab7e6b8245357993f47e366dcea
T.J <t.ruggles@live.ca>
2018-12-23 00:33:56 -0500

    Adding about component

---
+++ src/components/about.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>About Me</h1>
+    <hr />
+    <div>
+      <p>
+        Hi, I'm T.J., a front-end developer that loves building web applications, usually with React. I'm passionate about learning new technologies, as well as focused on constantly working to improve my current skill set. In this section you can preview some of the tools that I'm familiar with. Down below you can view some of the things I've worked on as well as fill out a form to get in contact with me.
+      </p>
+    </div>
+    <div>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+      <img src='#' alt='logo'/>
+    </div>
+  </div>
+);
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from './../components/header'
 import HeroBanner from './../components/hero-banner'
+import About from './../components/about'
 
 export default () => (
   <div>
     <Header></Header>
     <HeroBanner></HeroBanner>
+    <About></About>
   </div>
 );
