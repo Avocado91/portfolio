028ea9a78dad8f0f03915814a086c5bb80894e4f
T.J <t.ruggles@live.ca>
2019-02-12 16:56:28 -0500

    Adding layout for projects component

---
+++ src/components/projects.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Projects = () => (
+  <div>
+    <h1>Projects</h1>
+    <hr />
+    <div>
+      <div>
+        <h2>Title</h2>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+      </div>
+      <div>
+        <img srx=''></img>
+      </div>
+      <div>
+        <p>Description goes here</p>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <h2>Title2</h2>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+      </div>
+      <div>
+        <img srx=''></img>
+      </div>
+      <div>
+        <p>Description goes here</p>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <h2>Title3</h2>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+      </div>
+      <div>
+        <img srx=''></img>
+      </div>
+      <div>
+        <p>Description goes here</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Projects;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from './../components/header'
 import HeroBanner from './../components/hero-banner'
 import About from './../components/about'
+import Projects from './../components/projects'
 import Footer from './../components/footer'
 
 export default () => (
@@ -9,6 +10,7 @@ export default () => (
     <Header></Header>
     <HeroBanner></HeroBanner>
     <About></About>
+    <Projects></Projects>
     <Footer></Footer>
   </div>
 );
