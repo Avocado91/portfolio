c30d9543626be5d67fe9e619521336ad728032a4
T.J <t.ruggles@live.ca>
2019-02-15 18:23:03 -0500

    Adding hero banner background image

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import heroBkgImage from './../images/hero-bkg.jpg';
+import './../styles/components/hero-banner.css';
 
 const HeroBanner = () => (
-  <div>
-    <img src='#' alt='logo' />
+  <div className='hero-banner'>
+    <img className='hero-banner__image' src={heroBkgImage} alt='teal background' />
     <h1>T.J. Ruggles | Front-End Developer</h1>
     <h3>I'm a Web Developer based out of Ottawa, Canada</h3>
     <p>Learn more about what I do</p>
