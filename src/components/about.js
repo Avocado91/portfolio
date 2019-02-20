9cc69b1ee51961dc6875ced2e4d211fe260c4773
T.J <t.ruggles@live.ca>
2019-02-19 23:27:39 -0500

    Setting up section and wrapper styling components

--- src/components/about.js
+++ src/components/about.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './../styles/components/about.css';
+import './../styles/components/wrapper.css';
+import './../styles/components/section.css';
 
 const About = () => (
-  <div className='about' id='about'>
-    <h1>About Me</h1>
-    <hr />
-    <div>
-      <p>
-        Hi, I'm T.J., a front-end developer that loves building web applications, usually with React. I'm passionate about learning new technologies, as well as focused on constantly working to improve my current skill set. In this section you can preview some of the tools that I'm familiar with. Down below you can view some of the things I've worked on as well as fill out a form to get in contact with me.
-      </p>
-    </div>
-    <div>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
-      <img src='#' alt='logo'/>
+  <div className='wrapper'>
+    <div className='section' id='about'>
+      <h1 className='section__title'>About</h1>
+      <hr />
+      <div>
+        <p>
+          Hi, I'm T.J., a front-end developer that loves building web applications, usually with React. I'm passionate about learning new technologies, as well as focused on constantly working to improve my current skill set. In this section you can preview some of the tools that I'm familiar with. Down below you can view some of the things I've worked on as well as fill out a form to get in contact with me.
+        </p>
+      </div>
+      <div>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+        <img src='#' alt='logo'/>
+      </div>
     </div>
   </div>
 );
