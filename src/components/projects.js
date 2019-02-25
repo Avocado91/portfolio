5507f05b2298785d8bc10a9827248c51ac3ba49e
T.J <t.ruggles@live.ca>
2019-02-24 20:30:54 -0500

    Setting up transition effects for project hover

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,46 +8,52 @@ const Projects = () => (
       <h1 className='section__title'>Projects</h1>
       <hr className='section__hr' />
       <div className='projects'>
-
-
-
         <div className='project'>
+          <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
           <div className='project__overlay-container'>
             <h3 className='project__title'>Project Name</h3>
             <p className='project__tools'>Project tools go here</p>
             <button className='project__button'>Learn More</button>
           </div>
-          <div className='project__img-container project__img-container--test'>
-            <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
-          </div>
         </div>
-
-
-
-
         <div className='project'>
-          <div className='project__img-container'>
-            <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <div className='project__overlay-container'>
+            <h3 className='project__title'>Project Name</h3>
+            <p className='project__tools'>Project tools go here</p>
+            <button className='project__button'>Learn More</button>
           </div>
         </div>
         <div className='project'>
-          <div className='project__img-container'>
-            <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <div className='project__overlay-container'>
+            <h3 className='project__title'>Project Name</h3>
+            <p className='project__tools'>Project tools go here</p>
+            <button className='project__button'>Learn More</button>
           </div>
         </div>
         <div className='project'>
-          <div className='project__img-container'>
-            <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <div className='project__overlay-container'>
+            <h3 className='project__title'>Project Name</h3>
+            <p className='project__tools'>Project tools go here</p>
+            <button className='project__button'>Learn More</button>
           </div>
         </div>
         <div className='project'>
-          <div className='project__img-container'>
-            <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <div className='project__overlay-container'>
+            <h3 className='project__title'>Project Name</h3>
+            <p className='project__tools'>Project tools go here</p>
+            <button className='project__button'>Learn More</button>
           </div>
         </div>
         <div className='project'>
-          <div className='project__img-container'>
-            <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
+          <div className='project__overlay-container'>
+            <h3 className='project__title'>Project Name</h3>
+            <p className='project__tools'>Project tools go here</p>
+            <button className='project__button'>Learn More</button>
           </div>
         </div>
       </div>
