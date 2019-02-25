c776faf66b595754a62ba1ccbb4a4c8541fb5e4d
T.J <t.ruggles@live.ca>
2019-02-24 19:47:07 -0500

    Adding and styling project overlay

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,11 +8,23 @@ const Projects = () => (
       <h1 className='section__title'>Projects</h1>
       <hr className='section__hr' />
       <div className='projects'>
+
+
+
         <div className='project'>
-          <div className='project__img-container'>
+          <div className='project__overlay-container'>
+            <h3 className='project__title'>Project Name</h3>
+            <p className='project__tools'>Project tools go here</p>
+            <button className='project__button'>Learn More</button>
+          </div>
+          <div className='project__img-container project__img-container--test'>
             <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
           </div>
         </div>
+
+
+
+
         <div className='project'>
           <div className='project__img-container'>
             <img className='project__img' src='https://estampasuenos.cl/wp-content/uploads/2017/10/descarga3.png' alt='image of project' />
