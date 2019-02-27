2ae1c45b7199881b33c24c9892465ac13e26e469
T.J <t.ruggles@live.ca>
2019-02-27 18:05:40 -0500

    adding social link targets

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,17 +8,17 @@ const Footer = () => (
   <div className='section section--footer'>
     <div className='wrapper'>
       <div className='social'>
-        <a className='social__link' href='#'>
+        <a className='social__link' href='https://github.com/truggles85' target='_blank'>
           <div className='social__icon'>
             <i className='fab fa-github'></i>
           </div>
         </a>
-        <a className='social__link' href='#'>
+        <a className='social__link' href='https://www.linkedin.com/in/t-j-ruggles-0a12a1138/' target='_blank'>
           <div className='social__icon'>
             <i className='fab fa-linkedin-in'></i>
           </div>
         </a>
-        <a className='social__link' href='#'>
+        <a className='social__link' href='https://www.facebook.com/struggs' target='_blank'>
           <div className='social__icon'>
             <i className='fab fa-facebook-f'></i>
           </div>
