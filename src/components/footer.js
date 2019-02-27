b54315a18a859e59f6284f03c27d1120e3ba5791
T.J <t.ruggles@live.ca>
2019-02-27 18:01:18 -0500

    Finishing footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,21 +7,29 @@ import './../styles/components/social-link.css';
 const Footer = () => (
   <div className='section section--footer'>
     <div className='wrapper'>
-      <div className='social-container'>
-        <div className='social-link'>
-          <a href='#'><i className='fab fa-github'></i></a>
-        </div>
-        <div className='social-link'>
-          <a href='#'><i className='fab fa-linkedin-in'></i></a>
-        </div>
-        <div className='social-link'>
-          <a href='#'><i className='fab fa-facebook-f'></i></a>
-        </div>
-        <div className='social-link'>
-          <a href='mailto:t.ruggles@live.ca'><i className='fas fa-envelope'></i></a>
-        </div>
+      <div className='social'>
+        <a className='social__link' href='#'>
+          <div className='social__icon'>
+            <i className='fab fa-github'></i>
+          </div>
+        </a>
+        <a className='social__link' href='#'>
+          <div className='social__icon'>
+            <i className='fab fa-linkedin-in'></i>
+          </div>
+        </a>
+        <a className='social__link' href='#'>
+          <div className='social__icon'>
+            <i className='fab fa-facebook-f'></i>
+          </div>
+        </a>
+        <a className='social__link' href='mailto:t.ruggles@live.ca'>
+          <div className='social__icon'>
+            <i className='fas fa-envelope'></i>
+          </div>
+        </a>
       </div>
-      <div>T.J. Ruggles <i class="far fa-copyright"></i> 2018</div>
+      <div className='copyright'>T.J. Ruggles <i class="far fa-copyright"></i> 2018</div>
     </div>
   </div>
 );
