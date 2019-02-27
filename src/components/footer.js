31517a9e78c4313268a5905002406cc3d6edd0ef
T.J <t.ruggles@live.ca>
2019-02-26 22:43:19 -0500

    Styling basic elements for footer section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './../styles/components/footer.css';
+import './../styles/components/section.css';
+import './../styles/components/wrapper.css';
+import './../styles/components/social-link.css';
 
 const Footer = () => (
-  <div>
-    <div>
-      <a href='#'>Github</a>
-      <a href='#'>Linkedin</a>
-      <a href='#'>contact</a>
+  <div className='section section--footer'>
+    <div className='wrapper'>
+      <div className='social-container'>
+        <div className='social-link'>
+          <a href='#'><i className='fab fa-github'></i></a>
+        </div>
+        <div className='social-link'>
+          <a href='#'><i className='fab fa-linkedin-in'></i></a>
+        </div>
+        <div className='social-link'>
+          <a href='#'><i className='fab fa-facebook-f'></i></a>
+        </div>
+        <div className='social-link'>
+          <a href='mailto:t.ruggles@live.ca'><i className='fas fa-envelope'></i></a>
+        </div>
+      </div>
+      <div>T.J. Ruggles <i class="far fa-copyright"></i> 2018</div>
     </div>
-    <div>T.J. Ruggles - &copy 2018</div>
   </div>
 );
 
