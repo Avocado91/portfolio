01c62d71eac89e9e0b429bcb5f4d093ba2d010a5
T.J <t.ruggles@live.ca>
2019-03-06 23:39:48 -0500

    Finishing overlay styles

--- src/components/overlay.js
+++ src/components/overlay.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import './../styles/components/overlay.css';
 import './../styles/components/wrapper.css';
+import './../styles/components/button.css';
 
 const Overlay = () => (
   <div className='overlay'>
@@ -13,6 +14,20 @@ const Overlay = () => (
           <h3 className='section__subtitle'>Subtitle Goes Here</h3>
           <hr className='section__hr section__hr--overlay' />
           <p className='overlay__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue mi id auctor aliquam. Praesent quis purus eros. Integer at massa tempor, tincidunt dolor eu, bibendum arcu. Quisque viverra nibh sit amet felis faucibus, et eleifend enim porta. Fusce quis augue egestas leo tincidunt scelerisque ut convallis diam. Curabitur ut viverra elit.</p>
+        </div>
+        <div className='overlay__button-container'>
+          <a className='overlay__link' href='#' target='_blank'>
+            <button className='button button--overlay'>
+              <i className='button__icon fab fa-github'></i>
+              <span>Github</span>
+            </button>
+          </a>
+          <a className='overlay__link' href='#' target='_blank'>
+            <button className='button button--overlay'>
+              <i className='button__icon fas fa-external-link-alt'></i>
+              <span>Live Site</span>
+            </button>
+          </a>
         </div>
       </div>
     </div>
