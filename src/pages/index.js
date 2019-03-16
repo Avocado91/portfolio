5ebbabc7d339df48a0a0af96adb8c535685dc665
T.J <t.ruggles@live.ca>
2019-03-16 03:32:07 -0400

    Fixing case sensitive import netlify build fail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import HeroBanner from './../components/hero-banner'
-import About from './../components/about'
+import HeroBanner from '../components/Hero-banner'
+import About from '../components/About'
 import Projects from '../components/Projects'
-import Contact from './../components/contact'
-import Footer from './../components/footer'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 import 'normalize.css'
 import '../styles/styles.scss'
